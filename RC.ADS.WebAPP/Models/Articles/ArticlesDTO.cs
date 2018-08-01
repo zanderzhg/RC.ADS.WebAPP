e0943fe10c52e2f99ef93be8afdae4b61d6fc59d
陈成 <chencheng1990@hotmail.com>
2018-08-01 22:33:07 +0800

    添加模型

---
+++ RC.ADS.WebAPP/Models/Articles/ArticlesDTO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RC.ADS.WebAPP.Models.Articles
+{
+    public class ArticlesDTO
+    {
+        
+        public string Id { get; set; }
+        public string ArticleName { get; set; }
+        public string ArticleContent { get; set; }
+        public string ArticleIco { get; set; }
+        public string ArticleImage { get; set; }
+        public int ArticleIndex { get; set; }
+        public string ArticleTypeId   { get; set; }
+        public string ArticleTypeName { get; set; }
+    }
+}
