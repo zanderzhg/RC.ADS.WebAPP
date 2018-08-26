082f21023f44204d184d1d5d4175674a40eaea8e
陈成 <1750453097@qq.com>
2018-08-26 09:22:42 +0800

    待完成带参数二维码

---
+++ RC.ADS.Data/Migrations/20180821080420_addtableTopItem.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RC.ADS.Data.Migrations
+{
+    public partial class addtableTopItem : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TopupItems",
+                columns: table => new
+                {
+                    Id = table.Column<string>(nullable: false),
+                    TopupItemName = table.Column<string>(nullable: true),
+                    Price = table.Column<int>(nullable: false),
+                    Describe = table.Column<string>(nullable: true),
+                    IsDalete = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TopupItems", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TopupItems");
+        }
+    }
+}
